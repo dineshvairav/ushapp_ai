60b60ca1a1705db9271a275ae1412dc97c4920c3
prabha dinesh <ushaagency1960@gmail.com>
2025-06-17 03:33:11 +0000

    dev update

---
+++ functions/lib/test-import.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const auth_1 = require("firebase-functions/v2/auth");
+console.log(auth_1.onUserCreated);
+//# sourceMappingURL=test-import.js.map
